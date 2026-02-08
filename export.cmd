a470e85df6851101226df03ec8984334eed9a6ae
Aadi Deepchand <aadideepchand8254@gmail.com>
2026-02-08 23:35:13 +0000

    Updated export script and added .gitignore

--- export.cmd
+++ export.cmd
@@ -1,6 +1,11 @@
 setlocal enabledelayedexpansion
 
+del export\logbook.pdf
+
 set FILES=
 for %%F in (entries\*.md) do set FILES=!FILES! %%F
 
-pandoc README.md %FILES% -o export\logbook.pdf
+pandoc README.md %FILES% ^
+  --metadata-file=pandoc.yaml ^
+  --include-in-header=header.tex ^
+  -o export\logbook.pdf
